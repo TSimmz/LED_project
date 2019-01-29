ba6c0f2bb2a1062e426565676ecfeb634c01406b
Tyler Simoni <tyler.simoni@gmail.com>
2019-01-29 09:12:10 -0500

    Started web ui using webpy module

---
+++ led_web/index.py
@@ -0,0 +1,16 @@
+import web, cgi, settings
+import simplejson
+
+urls = (
+    '/', 'index'
+)
+
+app     = web.application(urls, globals())
+render  = web.template.render(settings.TEMPLATE_FOLDER, base='base')
+
+class index:
+    def GET(self):
+        return render.index()
+
+if __name__ == '__main__':
+    app.run()
